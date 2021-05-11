949993d8cdb853dae3c08d1e0fbe052bb5720d77
Chris Boyle <cthboyle6@gmail.com>
2021-05-11 12:22:20 -0400

    COMPLETED user action view single task details TODO: user action add new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # view all tasks
   get '/tasks', to: 'tasks#index'
+
+  # view single task details
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
